ec769bd4f80fbc9c74f7648e77f1e18497d7d1c9
tocky247 <s14591jt@sfc.keio.ac.jp>
2014-11-03 20:41:16 +0900

    lecture06

---
+++ addmachine.js
@@ -0,0 +1,27 @@
+var showAnswer = function(message){
+  var output = document.querySelector("#answer");
+  output.value = message;
+};
+
+var add = function(event){
+  var inputA = document.querySelector("#input-a");
+  var inputB = document.querySelector("#input-b");
+ 
+  var a = Number(inputA.value);
+  var b = Number(inputB.value);
+ 
+  var answer = a + b;
+ 
+  console.log("helloWorld: start");
+  showAnswer(answer);
+  console.log("helloWorld: end");
+};
+ 
+var boot = function(event){
+  console.log("boot: start");
+  var button = document.querySelector("button");
+  button.addEventListener("click", add);
+  console.log("boot: end");
+};
+ 
+window.addEventListener("load", boot);
